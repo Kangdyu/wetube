1b7cb3956ce59c526d8cfd84146f910e61702c84
Kangdyu <kangdyu99@gmail.com>
2020-09-02 20:36:42 +0900

    configure mongodb & mongoose

--- db.js
+++ db.js
@@ -1,50 +1,26 @@
-export const videos = [
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.connect(
+  process.env.MONGO_URL, 
   {
-    id: 234124,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: 213154122,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 12314,
-      name: "Kangdyu",
-      email: "kangdyu99@gmail.com"
-    }
-  },
-  {
-    id: 123,
-    title: "Video super",
-    description: "This is something I love",
-    views: 213154122,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 12314,
-      name: "Kangdyu",
-      email: "kangdyu99@gmail.com"
-    }
-  },
-  {
-    id: 234124,
-    title: "Video nice",
-    description: "This is something I love",
-    views: 213154122,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 12314,
-      name: "Kangdyu",
-      email: "kangdyu99@gmail.com"
-    }
-  },
-  {
-    id: 234124,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: 213154122,
-    videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 12314,
-      name: "Kangdyu",
-      email: "kangdyu99@gmail.com"
-    }
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
   }
-]
+);
+
+const db = mongoose.connection;
+
+const openHandler = () => {
+  console.log("connected to DB");
+}
+
+const errorHandler = (error) => {
+  console.log(`Error on DB Connection: ${error}`);
+}
+
+db.once("open", openHandler);
+db.on("error", errorHandler);
