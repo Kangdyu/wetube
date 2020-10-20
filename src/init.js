549315fe208f38c45d9356181fd00d5a01a17dd1
Kangdyu <kangdyu99@gmail.com>
2020-10-20 18:38:02 +0900

    fix dev:server

--- src/init.js
+++ src/init.js
@@ -10,7 +10,7 @@ dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const listenHandler = () => {
-  console.log(`Listening on: http://localhost:${PORT}`)
-}
+  console.log(`Listening on: http://localhost:${PORT}`);
+};
 
 app.listen(PORT, listenHandler);
